57d068e2166f60cd2837292b5b2f83a0be74cee4
Aleksander Mosolov <agmosolov@icloud.com>
2023-03-24 17:47:29 +0300

    UI elements + outlets and actions

--- MosolovLessonFour/ViewController.swift
+++ MosolovLessonFour/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var colorDisplayView: UIView!
+   
+    
+    @IBOutlet var redColorLabel: UISlider!
+    @IBOutlet var greenColorLabel: UILabel!
+    @IBOutlet var blueColorLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func changeRedSlider(_ sender: UISlider) {
+    }
+    
+    @IBAction func changeGreenSlider(_ sender: UISlider) {
+    }
+    
+    @IBAction func changeBlueSlider(_ sender: UISlider) {
+    }
+    
+    
+    
 }
 
